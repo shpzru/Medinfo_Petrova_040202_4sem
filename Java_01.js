1fe85395f3e6dd7a020b1b2dbcd8e38b2788b8ce
shpzru <pochti.kak.andreii@gmail.com>
2025-06-23 20:48:30 +0300

    Add files via upload

---
+++ Java_01.js
@@ -0,0 +1,11 @@
+'Привет, Java!'
+var MyThing;
+MyThing=5;
+MyThing="это строка";
+var numberNine = 9;
+var stringNine = "9";
+numberNine + numberNine;
+stringNine + stringNine;
+var greeting = "Привет, ";
+var myName = "shpz";
+greeting + myName;
